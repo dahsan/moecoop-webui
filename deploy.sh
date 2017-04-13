cfef645ae2b5af298a467235439a1a6dc5efda76
もじょんぬ <moe.e.coop@gmail.com>
2017-04-14 03:53:42 +0900

    デプロイスクリプトを修正

--- deploy.sh
+++ deploy.sh
@@ -9,10 +9,9 @@ echo $MOECOOP_DOMAIN > public/CNAME
 
 cd public
 
-git config user.email 'coop.e.moe@gmail.com'
-git config user.name 'Travis CI'
-
 git init
+git config user.name 'Travis CI'
+git config user.email 'coop.e.moe@gmail.com'
 git add .
 git commit -m "Deploy to Github Pages"
 git push -f -q "https://${GH_TOKEN}@github.com/coop-mojo/moecoop-webui.git" master:gh-pages > /dev/null 2>&1
