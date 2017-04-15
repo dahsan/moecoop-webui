1c6742634967b9a24a302c2792aed7b54561a840
もじょんぬ <moe.e.coop@gmail.com>
2017-04-15 11:51:02 +0900

    デプロイスクリプトから、ユーザー依存の部分を削除

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ cd dist
 
 git init
 git config user.name 'Travis CI'
-git config user.email 'coop.e.moe@gmail.com'
+git config user.email "$MOECOOP_MAIL"
 git add -A
 git commit -m "Deploy to Github Pages"
-git push -f -q "https://${GH_TOKEN}@github.com/coop-mojo/moecoop-webui.git" master:gh-pages > /dev/null 2>&1
+git push -f -q "https://${GH_TOKEN}@github.com/${MOECOOP_REPO}.git" master:gh-pages > /dev/null 2>&1
