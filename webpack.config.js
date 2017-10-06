2816a4de7a59df75644ff88922ad4a7a459e2c95
もじょんぬ <moe.e.coop@gmail.com>
2017-10-06 22:23:54 +0900

    babal-preset-latest の代わりに babel-preset-env を使うようにした

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,12 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        exclude: /node_modules/
+        exclude: /node_modules/,
+        options: {
+          presets: [
+            ['env', {'modules': false}]
+          ]
+        }
       },
       {
         test: /\.(png|svg|ico)$/,
