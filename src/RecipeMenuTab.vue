fcb96c0d883428b2f722103943a800826001b1c0
もじょんぬ <moe.e.coop@gmail.com>
2017-12-16 14:11:22 +0900

    レシピ一括タブのオプション項目を明記

--- src/RecipeMenuTab.vue
+++ src/RecipeMenuTab.vue
@@ -4,7 +4,7 @@
     <v-stepper-header>
       <v-stepper-step step="1" :complete="step > 1" :editable="step > 1">アイテムを選ぶ</v-stepper-step>
       <v-divider></v-divider>
-      <v-stepper-step step="2" :complete="step > 2" :editable="step > 2">レシピ・使用素材を選ぶ</v-stepper-step>
+      <v-stepper-step step="2" :complete="step > 2" :editable="step > 2">レシピ・使用素材を選ぶ<small>オプション</small></v-stepper-step>
       <v-divider></v-divider>
       <v-stepper-step step="3" :complete="step > 3">作成する</v-stepper-step>
     </v-stepper-header>
