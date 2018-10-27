b24dc89d57cc15dcdc980119e60b0b7eb986affa
もじょんぬ <moe.e.coop@gmail.com>
2018-10-28 04:01:22 +0900

    webpack.config.js を整理

--- webpack.config.js
+++ webpack.config.js
@@ -7,85 +7,75 @@ var HtmlWebpackPlugin = require('html-webpack-plugin')
 
 var appName = '生協の知恵袋'
 
-module.exports = {
-  entry: './src/main.js',
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'build.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-        options: {
-          loaders: {
+module.exports = (env, argv) => {
+  return {
+    entry: './src/main.js',
+    output: {
+      path: path.resolve(__dirname, './dist'),
+      filename: 'build.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader',
+          options: {
+            loaders: {
+            }
+            // other vue-loader options go here
           }
-          // other vue-loader options go here
-        }
-      },
-      {
-        test: /\.css$/,
-        loader: 'style-loader!css-loader',
-      },
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.(png|svg|ico)$/,
-        loader: 'file-loader',
-        options: {
-          name: 'images/[name].[ext]?[hash]'
-        }
-      },
-      {
-        test: /\.styl$/,
-        loader: ['style-loader', 'css-loader', 'stylus-loader'],
-      },
-      {
-        test: /\.html$/,
-        loader: 'html-loader',
-      },
-    ]
-  },
-  resolve: {
-    alias: {
-      'vue$': 'vue/dist/vue.esm.js'
-    }
-  },
-  devServer: {
-    historyApiFallback: true,
-    noInfo: true
-  },
-  performance: {
-    hints: false
-  },
-  devtool: '#eval-source-map',
-  plugins: [
-    new VueLoaderPlugin(),
-    new FaviconsWebpackPlugin({
-      logo: './src/assets/moecoop.svg',
-      prefix: 'images/',
-      inject: true,
-      title: appName,
-    }),
-    new HtmlWebpackPlugin({
-      title: appName,
-      template: '!!html-loader!./index.html'
-    })
-  ]
-}
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
-  // http://vue-loader.vuejs.org/en/workflow/production.html
-  module.exports.plugins = (module.exports.plugins || []).concat([
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"'
+        },
+        {
+          test: /\.css$/,
+          loader: 'style-loader!css-loader',
+        },
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.(png|svg|ico)$/,
+          loader: 'file-loader',
+          options: {
+            name: 'images/[name].[ext]?[hash]'
+          }
+        },
+        {
+          test: /\.styl$/,
+          loader: ['style-loader', 'css-loader', 'stylus-loader'],
+        },
+        {
+          test: /\.html$/,
+          loader: 'html-loader',
+        },
+      ]
+    },
+    resolve: {
+      alias: {
+        'vue$': 'vue/dist/vue.esm.js'
       }
-    }),
-  ])
+    },
+    devServer: {
+      historyApiFallback: true,
+      noInfo: true
+    },
+    performance: {
+      hints: false
+    },
+    devtool: argv.mode == 'production' ? 'none' : 'eval-source-map',
+    plugins: [
+      new VueLoaderPlugin(),
+      new FaviconsWebpackPlugin({
+        logo: './src/assets/moecoop.svg',
+        prefix: 'images/',
+        inject: true,
+        title: appName,
+      }),
+      new HtmlWebpackPlugin({
+        title: appName,
+        template: '!!html-loader!./index.html'
+      })
+    ]
+  }
 }
