f94f7ca1280991411b47d4e146198bf4e4a24ce2
もじょんぬ <moe.e.coop@gmail.com>
2018-10-28 03:18:31 +0900

    不必要になった cross-env を削除

--- webpack.config.js
+++ webpack.config.js
@@ -77,18 +77,3 @@ module.exports = {
     })
   ]
 }
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
-  // http://vue-loader.vuejs.org/en/workflow/production.html
-  module.exports.plugins = (module.exports.plugins || []).concat([
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"'
-      }
-    }),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true
-    }),
-  ])
-}
